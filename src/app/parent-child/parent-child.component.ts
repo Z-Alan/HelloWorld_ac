62bdc5a8b928e3b16c7fc9aa2fbfcd3736ad9dda
G-alan <nqws1002@163.com>
2018-04-15 18:58:00 +0800

    #00002-1 [父子组件间的通信@Input和@Output] [避免为输入和输出属性指定别名. 同一个属性有两个名字（一个对内一个对外）很容易导致混淆。]

--- src/app/parent-child/parent-child.component.ts
+++ src/app/parent-child/parent-child.component.ts
@@ -12,16 +12,16 @@ export class ParentChildComponent implements OnInit {
    * 父子组件通信区域*/
   //  count : number = 0; 这个写法 tsLint会报红,tsLint 建议remove type annotation
   @Input() count = 0;
-  @Output() change: EventEmitter<number> = new EventEmitter<number>();
+  @Output() countChange: EventEmitter<number> = new EventEmitter<number>();
 
   increment() {
     this.count++;
-    this.change.emit(this.count);
+    this.countChange.emit(this.count);
   }
 
   decrement() {
     this.count--;
-    this.change.emit(this.count);
+    this.countChange.emit(this.count);
   }
 
   constructor() { }
